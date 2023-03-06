056c30a1ae43dbdfd38fcd3368e8bc7abd27d306
Milo C <75094742+whotf1@users.noreply.github.com>
2023-03-06 11:20:17 -0600

    complete database

--- Develop/client/src/js/database.js
+++ Develop/client/src/js/database.js
@@ -13,9 +13,48 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
+
+  try {
+    const db = await openDB("jate", 1); 
+
+    const transaction = db.transaction("jate", "readwrite"); // setting as read and write permissions
+
+    const store = transaction.objectStore("jate");
+
+    const req = store.put ({ id: 1, value: content});
+
+    const res = await req;
+
+    console.log("~~~Data saved to db success~~~", res.value);
+  }
+  catch(err){
+    console.log("~~~something went wrong :(... Heres some info: \n", err);
+  }
+} 
+
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {
+  try  {
+    const db = await openDB("pwaDB", 1);
+
+    const transaction = jateDB.transaction("pwaDB", "readonly"); //opening context in read only, we wont need to write to this file yet
+    
+    const store = tx.objectStore("pwaaDB");
+
+    const req = store.get(1);
+    const res = await req;
+    res 
+      ? console.log("Data retrieved succeess", res.value)
+      : console.log("could not retrieve data")
+
+    return res?.value
+  }
+
+  catch(err){
+    console.log("~~~ran into some error getting info from db: ~~~\n", err)
+  }
+}
 
 initdb();
